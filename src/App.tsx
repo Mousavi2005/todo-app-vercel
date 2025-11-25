c9b175596cc3422c5192cd759c2be0e523997d10
mousavi <mousavics2005@gmail.com>
2025-11-25 13:22:03 +0330

    fix background img

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     const isDark = useSelector((state: RootState) => state.darkMode.isDark)
     return (
         <div className={`${isDark? "bg-[hsl(235,21%,11%)]" : "bg-[hsl(234,39%,85%)]"} relative w-screen h-screen flex items-center justify-center`}>
-            <div className={`${isDark? "bg-[url('./bg-desktop-dark.jpg')]" : "bg-[url('./bg-desktop-light.jpg')]"} absolute z-0 top-0 left-0 w-full h-[40%] bg-cover bg-center bg-no-repeat`}></div>
+            <div className={`${isDark? "bg-[url('/bg-desktop-dark.jpg')]" : "bg-[url('/bg-desktop-light.jpg')]"} absolute z-0 top-0 left-0 w-full h-[40%] bg-cover bg-center bg-no-repeat`}></div>
 
             <Section></Section>
         </div>
